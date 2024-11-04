2fec6f2c05e6755b0483f156353b2747f0d5f80f
shreyansh-1911 <shreyansh19112004@gmail.com>
2024-11-04 12:26:25 +0530

    updated Front-end and back-end

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
         setError(''); // Reset error message
 
         try {
-            const response = await fetch('http://localhost:5000/api/login', {
+            const response = await fetch('http://localhost:8000/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -12,7 +12,7 @@ const Register = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:5000/register', {
+            const response = await fetch('http://localhost:8000/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
